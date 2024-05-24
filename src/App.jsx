86bd5742d4026e1f3593f3c8076c02fd30df5523
Monisharaja-s <monishasukumaran07@gmail.com>
2024-05-24 20:58:46 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 
 //export const API_URL ="https://jsonplaceholder.typicode.com/users"
-//export const API_URL =  "https://659935c9a20d3dc41cef6114.mockapi.io/Axios_Task_json";
 export const API_URL = "https://6650a5d3ec9b4a4a6032dd9d.mockapi.io/axios"
 
 function App() {
