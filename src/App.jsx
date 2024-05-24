c9181478c6742de2264c287f9e215031020a6417
Monisharaja-s <monishasukumaran07@gmail.com>
2024-05-24 21:00:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AppRoutes from './utils/AppRoutes'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 
-//export const API_URL ="https://jsonplaceholder.typicode.com/users"
 export const API_URL = "https://6650a5d3ec9b4a4a6032dd9d.mockapi.io/axios"
 
 function App() {
